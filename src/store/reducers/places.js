54d19058d85b3d906a48fb357767c14c446c85c9
WillyLu326 <luzh0326@gmail.com>
2017-12-20 22:13:46 -0500

    create reducer skeleton

---
+++ src/store/reducers/places.js
@@ -0,0 +1,13 @@
+const initState = {
+  places: [],
+  selectedPlace: null
+};
+
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
+
+export default reducer;
