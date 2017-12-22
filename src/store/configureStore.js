38783fa71c0048deb3097141eb2e1dd9d9243e0e
WillyLu326 <luzh0326@gmail.com>
2017-12-21 22:04:28 -0500

    update rootReducer

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,9 @@
 import { createStore, combineReducers } from 'redux';
 import { placeReducer } from './reducers/placesReducer';
 
-const rootReducer = combineReducers({placeReducer});
+const rootReducer = combineReducers({
+  placeReducer: placeReducer
+});
 
 const store = createStore(rootReducer);
 
