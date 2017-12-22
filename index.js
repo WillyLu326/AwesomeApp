4d0b76a59445921f5b7d1e961e3e505e5a96a055
WillyLu326 <luzh0326@gmail.com>
2017-12-21 22:00:22 -0500

    connect Redux to react native app

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './src/store/configureStore';
 
-AppRegistry.registerComponent('AwesomeApp', () => App);
+const Redux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+AppRegistry.registerComponent('AwesomeApp', () => Redux);
