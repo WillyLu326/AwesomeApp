527212d0485321b6199cbbf11055d37ba9ee8f12
WillyLu326 <luzh0326@gmail.com>
2017-12-21 21:57:32 -0500

    create store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,3 +2,7 @@ import { createStore, combineReducers } from 'redux';
 import { placeReducer } from './reducers/placesReducer';
 
 const rootReducer = combineReducers({placeReducer});
+
+const store = createStore(rootReducer);
+
+export default store;
